a0c3e87cd662f0697e7a4448ca4d6139c419700d
AndKurt <kurtenkov.as@gmail.com>
2022-07-29 14:17:30 +0300

    feat: setted rollup

--- rollup.config.js
+++ rollup.config.js
@@ -1,10 +1,19 @@
+import path from 'path'
 import peerDepsExternal from 'rollup-plugin-peer-deps-external'
 import resolve from '@rollup/plugin-node-resolve'
 import commonjs from '@rollup/plugin-commonjs'
 import typescript from 'rollup-plugin-typescript2'
 import postcss from 'rollup-plugin-postcss'
+import json from '@rollup/plugin-json'
+import alias from '@rollup/plugin-alias'
+import svgr from '@svgr/rollup'
+import url from '@rollup/plugin-url'
 
 const packageJson = require('./package.json')
+
+const resolvePath = (dir) => {
+	return path.resolve(__dirname, dir)
+}
 
 export default {
 	//input: 'src/index.ts',
@@ -23,11 +32,31 @@ export default {
 	],
 	plugins: [
 		peerDepsExternal(),
-		resolve(),
+		svgr(),
+		resolve({ preferBuiltins: true }),
 		commonjs(),
+		json(),
 		typescript({ useTsconfigDeclarationDir: true }),
+		url({
+			include: ['**/*.woff', '**/*.woff2', '**/*.ttf'],
+			limit: Infinity,
+			fileName: '[dirname][name][extname]',
+		}),
 		postcss({
 			extensions: ['.css'],
+		}),
+		alias({
+			entries: [
+				{ find: '@', replacement: resolvePath('./src') },
+				{ find: '@assets', replacement: resolvePath('./src/assets') },
+				{ find: '@components', replacement: resolvePath('./src/components') },
+				{ find: '@constants', replacement: resolvePath('./src/constants') },
+				{ find: '@logic', replacement: resolvePath('./src/logic') },
+				{ find: '@mock', replacement: resolvePath('./src/mock') },
+				{ find: '@styles', replacement: resolvePath('./src/styles') },
+				{ find: '@utils', replacement: resolvePath('./src/utils') },
+				{ find: '@interfaces', replacement: resolvePath('./src/interfaces') },
+			],
 		}),
 	],
 }
