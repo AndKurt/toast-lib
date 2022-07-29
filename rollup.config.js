a46ae679174b7138f170df0e380ffb4196d79906
AndKurt <kurtenkov.as@gmail.com>
2022-07-29 10:31:04 +0300

    test

--- rollup.config.js
+++ rollup.config.js
@@ -7,8 +7,8 @@ import postcss from 'rollup-plugin-postcss'
 const packageJson = require('./package.json')
 
 export default {
-	input: 'src/index.ts',
-	//input: 'src/index.tsx',
+	//input: 'src/index.ts',
+	input: 'src/index.tsx',
 	output: [
 		{
 			file: packageJson.main,
